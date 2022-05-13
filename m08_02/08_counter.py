b84e026b2f22c73b7a87a81e746c1111bdf7be65
krabaton <krabatua@gmail.com>
2022-05-13 18:43:21 +0300

    start 08_02

---
+++ m08_02/08_counter.py
@@ -0,0 +1,17 @@
+"""
+Реализовать функцию, которая возвращает n наиболее часто встречающихся и n наименее часто встречающихся чисел из файла
+"""
+
+from collections import Counter
+
+filename = 'numbers.txt'
+
+
+def num_counter(filename, n):
+
+    return None
+
+
+result = num_counter(filename, 10)
+
+print(result)
